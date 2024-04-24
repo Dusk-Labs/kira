952b932df924d5b22cf7d3c367adc4a1fadd63aa
IGI-111 <igi-111@protonmail.com>
2024-04-25 01:34:57 +0400

    New TouchArea based architecture

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use slint::LogicalPosition;
 use slint::Model;
 use slint::VecModel;
 use std::rc::Rc;
@@ -46,10 +47,6 @@ fn main() -> Result<(), slint::PlatformError> {
             dst: 2,
             dst_slot: 0,
             ty: LinkType::A,
-            detached_src: false,
-            detached_dst: false,
-            detached_x: 0.,
-            detached_y: 0.,
         },
         LinkData {
             src: 0,
@@ -57,10 +54,6 @@ fn main() -> Result<(), slint::PlatformError> {
             dst: 2,
             dst_slot: 1,
             ty: LinkType::B,
-            detached_src: false,
-            detached_dst: false,
-            detached_x: 0.,
-            detached_y: 0.,
         },
         LinkData {
             src: 0,
@@ -68,83 +61,177 @@ fn main() -> Result<(), slint::PlatformError> {
             dst: 2,
             dst_slot: 1,
             ty: LinkType::B,
-            detached_src: false,
-            detached_dst: false,
-            detached_x: 0.,
-            detached_y: 0.,
         },
     ]));
+    let floating = FloatingLinkData {
+        floating_state: FloatingState::None,
+        ..Default::default()
+    };
 
     let ui = AppWindow::new()?;
     ui.set_nodes(nodes.clone().into());
     ui.set_links(links.clone().into());
+    ui.set_floating(floating.clone().into());
 
-    ui.global::<LinkLogic>().on_try_attach_src({
+    ui.global::<LinkLogic>().on_new_link_from_output({
         let nodes = nodes.clone();
-        move |mut link| {
-            for (node_idx, node) in nodes.iter().enumerate() {
-                for (slot_idx, slot) in node.outputs.iter().enumerate() {
-                    let has_collision = {
-                        let slot_idx = f32::from(u8::try_from(slot_idx).unwrap());
-                        let detach_top = link.detached_y;
-                        let detach_bottom = link.detached_y + 10.;
-                        let detach_left = link.detached_x;
-                        let detach_right = link.detached_x + 10.;
-                        let output_top = node.y + 15. * slot_idx;
-                        let output_bottom = node.y + 15. * slot_idx + 10.;
-                        let output_left = node.x + node.width;
-                        let output_right = node.x + node.width + 10.;
-
-                        detach_left < output_right
-                            && detach_right > output_left
-                            && detach_top < output_bottom
-                            && detach_bottom > output_top
-                    };
-
-                    if has_collision && slot == link.ty {
-                        link.detached_src = false;
-                        link.src = node_idx.try_into().unwrap();
-                        link.src_slot = slot_idx.try_into().unwrap();
-                        return link;
+        let links = links.clone();
+        let ui = ui.as_weak();
+        move |node_idx, slot_idx| {
+            if let Some(slot_ty) = nodes
+                .row_data(node_idx as usize)
+                .and_then(|n| n.outputs.row_data(slot_idx as usize))
+            {
+                for i in 0..links.row_count() {
+                    if let Some(link) = links.row_data(i) {
+                        if link.src == node_idx && link.src_slot == slot_idx {
+                            let link = links.remove(i);
+                            ui.upgrade().unwrap().set_floating(FloatingLinkData {
+                                floating_state: FloatingState::DstAttached,
+                                node: link.dst,
+                                node_slot: link.dst_slot,
+                                ty: slot_ty,
+                                x: 0.,
+                                y: 0.,
+                            });
+                            return;
+                        }
                     }
                 }
+
+                ui.upgrade().unwrap().set_floating(FloatingLinkData {
+                    floating_state: FloatingState::SrcAttached,
+                    node: node_idx,
+                    node_slot: slot_idx,
+                    ty: slot_ty,
+                    x: 0.,
+                    y: 0.,
+                });
             }
-            link.detached_src = false;
-            link
         }
     });
-    ui.global::<LinkLogic>().on_try_attach_dst({
+    ui.global::<LinkLogic>().on_new_link_from_input({
         let nodes = nodes.clone();
-        move |mut link| {
-            for (node_idx, node) in nodes.iter().enumerate() {
-                for (slot_idx, slot) in node.inputs.iter().enumerate() {
-                    let has_collision = {
-                        let slot_idx = f32::from(u8::try_from(slot_idx).unwrap());
-                        let detach_top = link.detached_y;
-                        let detach_bottom = link.detached_y + 10.;
-                        let detach_left = link.detached_x;
-                        let detach_right = link.detached_x + 10.;
-                        let input_top = node.y + 15. * slot_idx;
-                        let input_bottom = node.y + 15. * slot_idx + 10.;
-                        let input_left = node.x - 10.;
-                        let input_right = node.x;
-
-                        detach_left < input_right
-                            && detach_right > input_left
-                            && detach_top < input_bottom
-                            && detach_bottom > input_top
-                    };
-
-                    if has_collision && slot == link.ty {
-                        link.detached_dst = false;
-                        link.dst = node_idx.try_into().unwrap();
-                        link.dst_slot = slot_idx.try_into().unwrap();
-                        return link;
+        let links = links.clone();
+        let ui = ui.as_weak();
+        move |node_idx, slot_idx| {
+            if let Some(slot_ty) = nodes
+                .row_data(node_idx as usize)
+                .and_then(|n| n.inputs.row_data(slot_idx as usize))
+            {
+                for i in 0..links.row_count() {
+                    if let Some(link) = links.row_data(i) {
+                        if link.dst == node_idx && link.dst_slot == slot_idx {
+                            let link = links.remove(i);
+                            ui.upgrade().unwrap().set_floating(FloatingLinkData {
+                                floating_state: FloatingState::SrcAttached,
+                                node: link.src,
+                                node_slot: link.src_slot,
+                                ty: slot_ty,
+                                x: 0.,
+                                y: 0.,
+                            });
+                            return;
+                        }
                     }
                 }
+                ui.upgrade().unwrap().set_floating(FloatingLinkData {
+                    floating_state: FloatingState::DstAttached,
+                    node: node_idx,
+                    node_slot: slot_idx,
+                    ty: slot_ty,
+                    x: 0.,
+                    y: 0.,
+                });
             }
-            link.detached_dst = false;
-            link
+        }
+    });
+    ui.global::<LinkLogic>().on_attach_link_to_input({
+        let nodes = nodes.clone();
+        let links = links.clone();
+        let ui = ui.as_weak();
+        move |node_idx, slot_idx| {
+            if let Some(slot_ty) = nodes
+                .row_data(node_idx as usize)
+                .and_then(|n| n.inputs.row_data(slot_idx as usize))
+            {
+                let floating = ui.upgrade().unwrap().get_floating();
+                if floating.ty == slot_ty && floating.floating_state == FloatingState::SrcAttached {
+                    links.push(LinkData {
+                        dst: node_idx,
+                        dst_slot: slot_idx,
+                        src: floating.node,
+                        src_slot: floating.node_slot,
+                        ty: floating.ty,
+                    });
+                }
+            }
+            ui.upgrade().unwrap().set_floating(FloatingLinkData {
+                floating_state: FloatingState::None,
+                ..Default::default()
+            });
+        }
+    });
+    ui.global::<LinkLogic>().on_attach_link_to_output({
+        let nodes = nodes.clone();
+        let links = links.clone();
+        let ui = ui.as_weak();
+        move |node_idx, slot_idx| {
+            if let Some(slot_ty) = nodes
+                .row_data(node_idx as usize)
+                .and_then(|n| n.outputs.row_data(slot_idx as usize))
+            {
+                let floating = ui.upgrade().unwrap().get_floating();
+                if floating.ty == slot_ty && floating.floating_state == FloatingState::DstAttached {
+                    links.push(LinkData {
+                        src: node_idx,
+                        src_slot: slot_idx,
+                        dst: floating.node,
+                        dst_slot: floating.node_slot,
+                        ty: floating.ty,
+                    });
+                }
+            }
+            ui.upgrade().unwrap().set_floating(FloatingLinkData {
+                floating_state: FloatingState::None,
+                ..Default::default()
+            });
+        }
+    });
+    ui.global::<MoveAreaLogic>().on_click_event_hack({
+        let ui = ui.as_weak();
+        move |x, y, evt| {
+            ui.upgrade().unwrap().window().dispatch_event(
+                slint::platform::WindowEvent::PointerPressed {
+                    position: LogicalPosition { x, y },
+                    button: evt.button,
+                },
+            );
+            ui.upgrade().unwrap().window().dispatch_event(
+                slint::platform::WindowEvent::PointerReleased {
+                    position: LogicalPosition { x, y },
+                    button: evt.button,
+                },
+            );
+        }
+    });
+    ui.global::<MoveAreaLogic>().on_update_floating_position({
+        let ui = ui.as_weak();
+        move |x, y| {
+            ui.upgrade().unwrap().set_floating(FloatingLinkData {
+                x,
+                y,
+                ..ui.upgrade().unwrap().get_floating()
+            });
+        }
+    });
+    ui.global::<MoveAreaLogic>().on_reset_floating_state({
+        let ui = ui.as_weak();
+        move || {
+            ui.upgrade().unwrap().set_floating(FloatingLinkData {
+                floating_state: FloatingState::None,
+                ..Default::default()
+            });
         }
     });
 
